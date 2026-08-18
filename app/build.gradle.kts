21ec074609ebda20e70191471d324716ce72491a
Juan Yunis <jpyunism@gmail.com>
2026-08-17 23:54:33 -0400

    chore: bump a v0.26.0 (refactor de arquitectura)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.jpyunism.ollamacloudusage"
         minSdk = 26
         targetSdk = 36
-        versionCode = 37
-        versionName = "0.25.0"
+        versionCode = 38
+        versionName = "0.26.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
