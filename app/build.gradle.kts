d2fb9ff98144b2089ad53b6075ff0822ee2a434f
Juan Yunis <jpyunism@gmail.com>
2026-09-12 08:04:16 -0300

    chore: bump versionCode 55 / v0.38.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.jpyunism.ollamacloudusage"
         minSdk = 26
         targetSdk = 36
-        versionCode = 54
-        versionName = "0.37.2"
+        versionCode = 55
+        versionName = "0.38.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
